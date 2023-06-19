2794f6cd95664b3f2f262a5cab5352602afd6705
Muccachino <lucas_staszewski@yahoo.de>
2023-06-19 13:50:40 +0200

    players are being created, marks can be set, game checks if winning condition is true.

--- main.js
+++ main.js
@@ -1,1 +1,146 @@
 "use strict";
+
+let turnCounter = 1;
+
+class Gameboard {
+  constructor() {
+    this.players = [];
+    this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  }
+  addPlayer(name) {
+    let player = new Player(name);
+    this.players.push(player);
+  }
+  checkWin() {
+    if (
+      (this.board[1] === this.board[2] &&
+        this.board[2] === this.board[3] &&
+        this.board[1] === this.board[3] &&
+        this.board[1] != 0) ||
+      (this.board[4] === this.board[5] &&
+        this.board[5] === this.board[6] &&
+        this.board[4] === this.board[6] &&
+        this.board[4] != 0) ||
+      (this.board[7] === this.board[8] &&
+        this.board[8] === this.board[9] &&
+        this.board[7] === this.board[9] &&
+        this.board[7] != 0) ||
+      (this.board[1] === this.board[4] &&
+        this.board[4] === this.board[7] &&
+        this.board[1] === this.board[7] &&
+        this.board[1] != 0) ||
+      (this.board[2] === this.board[5] &&
+        this.board[5] === this.board[8] &&
+        this.board[2] === this.board[8] &&
+        this.board[2] != 0) ||
+      (this.board[3] === this.board[6] &&
+        this.board[6] === this.board[9] &&
+        this.board[3] === this.board[9] &&
+        this.board[3] != 0) ||
+      (this.board[1] === this.board[5] &&
+        this.board[5] === this.board[9] &&
+        this.board[1] === this.board[9] &&
+        this.board[1] != 0) ||
+      (this.board[3] === this.board[5] &&
+        this.board[5] === this.board[7] &&
+        this.board[7] === this.board[9] &&
+        this.board[3] != 0)
+    ) {
+      showWinner();
+    }
+  }
+}
+
+const gameboard = new Gameboard();
+
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const getPlayerNames = () => {
+  let playerOne = document.querySelector("#playerOne");
+  playerOne = playerOne.value;
+  let playerTwo = document.querySelector("#playerTwo");
+  playerTwo = playerTwo.value;
+  checkPlayerInput(playerOne, playerTwo);
+};
+
+const checkPlayerInput = (playerOne, playerTwo) => {
+  if (playerOne === "" || playerTwo === "") {
+    const message = document.querySelector("#message");
+    message.innerHTML = "Please insert both player names!";
+  } else {
+    createPlayers(playerOne, playerTwo);
+  }
+};
+
+const createPlayers = (playerOne, playerTwo) => {
+  let one = gameboard.addPlayer(playerOne);
+  let two = gameboard.addPlayer(playerTwo);
+  console.log(gameboard.players[0], gameboard.players[1]);
+  console.log(gameboard.players);
+
+  let boardNameOne = document.querySelector("#boardNameOne");
+  let boardNameTwo = document.querySelector("#boardNameTwo");
+
+  boardNameOne.innerHTML = gameboard.players[0].name;
+  boardNameTwo.innerHTML = gameboard.players[1].name;
+};
+
+const setMark = (e) => {
+  let targetElement = e.target;
+  if (targetElement.matches(".square")) {
+    if (
+      turnCounter === 1 &&
+      gameboard.board[parseInt(targetElement.dataset.field)] === 0
+    ) {
+      createCircle(targetElement);
+      gameboard.board[parseInt(targetElement.dataset.field)] = 1;
+      gameboard.checkWin();
+      turnCounter = 2;
+    } else if (
+      turnCounter === 2 &&
+      gameboard.board[parseInt(targetElement.dataset.field)] === 0
+    ) {
+      createCross(targetElement);
+      gameboard.board[parseInt(targetElement.dataset.field)] = 2;
+      gameboard.checkWin();
+      turnCounter = 1;
+    }
+  }
+};
+
+const createCircle = (targetElement) => {
+  let circle = document.createElement("i");
+  circle.classList.add("fa-regular", "fa-circle");
+  targetElement.appendChild(circle);
+};
+
+const createCross = (targetElement) => {
+  let cross = document.createElement("i");
+  cross.classList.add("fa-solid", "fa-x");
+  targetElement.appendChild(cross);
+};
+
+const showWinner = () => {
+  let winnerOutput = document.querySelector("#winnerOutput");
+  if ((turnCounter = 1)) {
+    winnerOutput.innerHTML = gameboard.players[0].name + " is the winner";
+  } else {
+    (winnerOutput.innerHTML = gameboard.players[1].name), " is the winner";
+  }
+};
+
+const listenToClick = (() => {
+  const allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square) => {
+    square.addEventListener("click", setMark);
+  });
+})();
+
+const gameStart = (() => {
+  const startGame = document.querySelector("#startGame");
+  startGame.addEventListener("click", getPlayerNames);
+})();
